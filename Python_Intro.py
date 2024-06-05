f866e520014b38286fecb2b10c94979b160a66dc
Wamae Symon <symonwamae.k@gmail.com>
2024-06-05 03:37:14 -0600

    This is my real Demo

---
+++ Python_Intro.py
@@ -0,0 +1,29 @@
+#correct identation
+age = 18
+# If Age >= 18:
+# print ("You are a adult")
+# Else if Age>12
+     #print("You are a teenager")
+# Else if
+#print("you are a child")
+
+#list comprehension
+squares = [x**2 for x in range (10)]
+print (squares)
+# for loop 
+#for in range (5):
+#print (f"i is i")
+#while loop
+count = 0
+while count < 5:
+     print (f"count is[count]")
+     count +=1
+Function 
+def add (a,b):
+    return a+b
+results = add(5,8)
+print(results)
+# functions
+# lambda function
+multiply =lambda x, y: x*y
+
